950a7b9271df80782fbe2a66e56f05b28271481b
Tsarevich01 <pytinvovan@mail.ru>
2018-06-29 16:46:29 +0400

    запилил немного функционала, сделал макет водителей и машин, так же добавлен модуль добавления водителей(остался вывод)

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template, flash, redirect, url_for
+import datetime
+
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 
@@ -7,17 +9,10 @@ from app.forms import LoginForm
 @app.route('/index')
 def index():
     user = {'username': 'man'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
+    historycar = [
+
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=historycar)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -28,3 +23,61 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html',  title='Авторизация', form=form)
+
+
+@app.route('/akts')
+def akts():
+    return render_template('akts.html', title = 'Акты')
+
+@app.route('/drivers')
+def drivers():
+    driveuser = {'fio': 'ФИО',
+                 'seriya': 'Серия паспорта',
+                 'nomer': 'Номер паспорта'}
+    drive = [
+        {
+            'fio': {'familiya': 'Иванов',
+                    'imya': 'Иван',
+                    'otchestvo': 'Иванович'},
+            'seriya': '2001',
+            'nomer': '123456',
+        },
+    ]
+    return render_template('drivers.html', title = 'Водители', drive = drive, driveuser=driveuser)
+
+
+@app.route('/add_drive', methods=['POST', 'GET'])
+def add_drive():
+    if request.method == 'POST':
+        drive = {
+            'fio': request.form['fio'],
+            'seriya': request.form['seriya'],
+            'nomer': request.form['body'],
+
+        }
+        drivers.append(drive)
+
+        flash('Новый пост добавлен')
+
+        return redirect(url_for('drivers'))
+    else:
+        return render_template('add_drive.html')
+
+
+
+@app.route('/car')
+def car():
+    usercar = {
+        'marka': 'Марка',
+        'model': 'Модель',
+        'znak': 'Номерной знак'
+    }
+    car = [
+        {
+            'marka': 'Lada',
+            'model': 'Kalina',
+            'znak': "С777СС73",
+        },
+    ]
+    return render_template('car.html', title = 'Машины', car = car, usercar=usercar)
+
